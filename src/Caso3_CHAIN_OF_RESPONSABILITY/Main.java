db12349717369d42b7d8b632c02b77bbc03974a5
José Navarro <30849623+JoseNA12@users.noreply.github.com>
2018-11-10 22:36:38 -0600

    Commit inicial

---
+++ src/Caso3_CHAIN_OF_RESPONSABILITY/Main.java
@@ -0,0 +1,9 @@
+package Caso3_CHAIN_OF_RESPONSABILITY;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/Caso4_Patron/Main.java
@@ -0,0 +1,9 @@
+package Caso4_Patron;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
